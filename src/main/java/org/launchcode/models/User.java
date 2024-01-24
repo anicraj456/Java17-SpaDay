c249896c8fd2e287001c9c34a74c1a997324b67b
anicraj456 <anicraj123@gmail.com>
2024-01-24 16:42:52 -0600

    studio working on it

--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -1,14 +1,21 @@
 package org.launchcode.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
 public class User {
+    @NotEmpty(message="Please enter Username")
+    @Size(min=5, max=15, message="Username must be 5 to 15 charaters")
     private String username;
+    @Email(message="Invaild email")
     private String email;
+    @NotEmpty(message="Please enter Password")
+    @Size(max=6, message="Password can have upto 6 charaters")
     private String password;
 
     public User() {
-
     }
-
     public User(String username, String email, String password) {
         this();
         this.username = username;
